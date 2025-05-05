c84f4a88025a4f3e44a9b46d45a4bada89ad0f89
abdulrhman-mustafa <abdulrhmanmustafa00@gmail.com>
2025-05-05 23:24:59 +0300

    Import image in Title.jsx for proper loading

--- src/Components/Title.jsx
+++ src/Components/Title.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Image from '/wish-list_3585145.png';
 
 const Title = () => {
     return (
         <>
             <div className="title">
                 <h1>Todo List</h1>
-                <img src="/wish-list_3585145.png" alt="Note Book" />
+                <img src={Image} alt="Note Book" />
             </div>
         </>
     );
